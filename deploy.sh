b3ba09c75a82158a13a4e875ac0b466c198c329d
ltg <ltg@bbangyanetworks.com>
2022-01-26 16:21:58 +0900

    second

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# 명령어 실패 시 script를 바로 종료시칸다.
+set -e
+
+# vue build
+npm run build
+
+cd dist
+
+#git init
+#git add -A
+#git commit -m 'New Deployment'
+#git push -f git@github.com-koallazon:koallazon/vue-resume.git master:resume-pages
+
+git add -A
+git commit -m $1
+git push origin/resume-page
+
+cd -
