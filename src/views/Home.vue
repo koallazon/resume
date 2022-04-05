d7b613a6aaba50197ef4af33300f89a9e4add14f
ltg <ltg@bbangyanetworks.com>
2022-04-05 22:47:30 +0900

    tailwind 적용

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home mx-auto bg-white rounded-xl shadow-lg">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
