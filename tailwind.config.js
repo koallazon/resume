259455b217afbb798b628a919b153a195f8ca73b
ltg <ltg@bbangyanetworks.com>
2022-04-05 23:01:54 +0900

    tailwind purge 속성 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,4 +11,9 @@ module.exports = {
     },
   },
   plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === "production",
+    content: ["components/**/*.vue", "views/**/*.vue"],
+  },
 };
