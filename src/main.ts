ff05b735a5b37d5afc9ffed5bc91af41afac62dd
ltg <ltg@bbangyanetworks.com>
2022-07-31 14:05:08 +0900

    커스텀 배포를 위한 업데이트 1

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./index.css";
-import VueGtag from "vue-gtag";
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+import "./index.css"
+import VueGtag from "vue-gtag"
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .use(VueGtag, {
-    config: { id: "G-BYCDE4J88X" },
-  })
-  .mount("#app");
+createApp(App).use(store).use(router).mount("#app")
+
+// .use(VueGtag, {
+//   config: { id: "G-BYCDE4J88X" },
+// })
